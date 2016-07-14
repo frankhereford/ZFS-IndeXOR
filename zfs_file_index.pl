794abcdaa009e2e7ec81c6057012cf588b380771
Frank Hereford <frank@frankhereford.com>
2016-07-14 13:01:01 -0500

    before i go taking the print statements out

--- zfs_file_index.pl
+++ zfs_file_index.pl
@@ -53,17 +53,17 @@ sub do_not_dwell_on_what_has_passed_away_or_what_is_yet_to_be
 
   %how_to_get_to_mordor = map { $_ => 1 } @filesystems; # make a map to mordor by um, mapping, the zfs array? Sure why not.
 
-  print Dumper \@depths, "\n";
+  #print Dumper \@depths, "\n";
   # so now we have this ....thing and we can go up through it backwards and know where we have been and were we ought not go.
 
   foreach my $depth (reverse(@depths))
     {
     foreach my $directory (@{$depth})
       {
-      print "\n\n\n";
-      print $directory, "\n";
-      print "Ready?\n";
-      <>;
+      #print "\n\n\n";
+      #print $directory, "\n";
+      #print "Ready?\n";
+      #<>;
       find({ wanted => \&where_is_my_gypsy_wife_tonight, preprocess => \&stubborn_as_hose_garbage_bags_that_time_cannot_decay },$directory);
       }
     }
@@ -79,9 +79,9 @@ sub where_is_my_gypsy_wife_tonight
 
   if (-d $name)
     {
-    my $id = &my_house_in_the_middle_of_the_street($name)
-    print $id, ": ", my_house_in_the_middle_of_the_street($name), "\n";
-    <>;
+    my $id = &my_house_in_the_middle_of_the_street($name);
+    print $id, ": ", $name, "\n";
+    #<>;
     #&insert_directory($name); # insert directory
     }
   #elsif (-e $name and -r $name)
@@ -130,176 +130,70 @@ sub occurrences
   return $matches;
   }
 
-exit;
-
-
-
-
-
-
-
-sub make_a_home
+sub my_house_in_the_middle_of_the_street
   {
-
-  }
-
-
-
-
-
-
-
-
-my $root = '/';
-
-
-my $sql;
-$sql = "insert into directories (parent, depth, name) values (?, ?, ?)";
-my $insert_directory = $db->prepare($sql);
-$sql = "select id, depth from directories where name = ? and parent = ?";
-my $get_directory = $db->prepare($sql);
-$sql = "insert into files (filename, directory) values (?, ?)";
-my $insert_file_stub = $db->prepare($sql);
-$sql = "select id, name, depth, parent from directories where id = ?";
-my $get_directory_build = $db->prepare($sql);
-$sql = "update files set size = ?, md5 = ? where id = ?";
-my $set_size_md5 = $db->prepare($sql);
-
-for (my $x = 0; $x < 1000; $x++)
-  {
-  my $sql = "select directory, filename, id from files where size IS NULL and md5 IS NULL and unusual is NULL order by id asc limit 0,1000";
-  my $query = $db->prepare($sql);
-  $query->execute();
-  while (my $file = $query->fetchrow_hashref)
+  #print "Welcome to zombocom\n";
+  my $path = shift;
+  #print $path, "\n";
+  my @path = split("/", $path);
+  my $zeroth_before_the_slash_nothing_lived_here_ignore_this_long_variable_the_compiler_hates_us_both = shift(@path);
+  my $directory = shift(@path);
+  #print Dumper \@path, "\n";
+  my $root_sql = "select id from directories where name = ? and depth = ?"; #" and parent = ?";
+  my $root_query = $db->prepare($root_sql);
+  #print "Args: '", $directory, "'\n";
+  $root_query->execute($directory, 0);
+  my $root_result = $root_query->fetchrow_hashref;
+  #print Dumper $root_result, "\n";
+  my $id;
+  if ($root_result->{'id'})
     {
-    #print $file->{'filename'}, "\n";
-    my @path = ($file->{'filename'});
-    $get_directory_build->execute($file->{'directory'});
-    my $d = $get_directory_build->fetchrow_hashref;
-    unshift(@path, $d->{'name'});
-    while ($d->{'depth'} != 0)
-      {
-      #print "2\n";
-      $get_directory_build->execute($d->{'parent'});
-      $d = $get_directory_build->fetchrow_hashref;
-      unshift(@path, $d->{'name'});
-      }
-    my $path = join('/', @path);
-
-    print $path, "\n";
-    next;
-    if (-e $path and -f $path)
-      {
-      #print "3\n";
-      my $size = -s $path;
-      #print "Size: ", $size, "\n";
-      #my $size = 0;
-      #print "4\n";
-      open (my $fileh, '<', $path) or die "Can't open '$path': $!";
-      #print "4.5\n";
-      binmode ($fileh);
-      my $md5 = Digest::MD5->new->addfile($fileh)->hexdigest;
-      close $fileh;
-      #print "5\n";
-      print 'Size: ', $size, '; MD5: ', $md5, "\n";
-      #$set_size_md5->execute($size, $md5, $file->{'id'});
-      }
-    else
-      {
-      my $sql = "update files set unusual = 1 where id = ?";
-      my $unusual = $db->prepare($sql);
-      #$unusual->execute($file->{'id'});
-      }
-    }
-  }
-
-
-
-sub find_files
-  {
-  $db->do('TRUNCATE files;');
-  $db->do('TRUNCATE directories;');
-  my $cmd = 'zfs list -o mountpoint -t filesystem';
-  open(my $zfs, "-|", $cmd);
-  <$zfs>;
-  close $zfs;
-
-
-
-  foreach my $path (<$zfs>) 
-    { 
-    chomp $path;
-    find(\&corkscrew,$path);
-    }
-
-  }
-
-sub corkscrew
-  {
-  my $name = $File::Find::name;
-  my $dir = $File::Find::dir;
-  my $base = $_;
-
-  my ($filename, $directory) = fileparse($name);
-
-  if (-d $name)
-    {
-    &insert_directory($name);
-    }
-  elsif (-e $name and -r $name)
-    {
-    if (0)
-      {
-      print "Name: ", $name, "\n";
-      print "Dir: ", $dir, "\n";
-      print "Base: ", $base, "\n";
-      print "Filename: ", $filename, "\n";
-      print "Directory: ", $directory, "\n";
-      }
-    &insert_file($name);
-    }
-  }
-
-sub insert_file
-  {
-  my $d = shift;
-  $d = substr($d, length($root));
-  my @path = split(/\//,$d);
-  shift(@path); # leading slash leaves empty zeroth register
-  my $filename = pop(@path);
-  my $parent = get_parent_id(\@path,0);
-  #print "Parent: ", $parent->{'id'}, "\n";
-  $insert_file_stub->execute($filename, $parent->{'id'});
-  }
-
-sub insert_directory
-  {
-  my $d = shift;
-  $d = substr($d, length($root));
-  my @path = split(/\//,$d);
-  shift(@path); # leading slash leaves empty zeroth register
-  if (scalar(@path))
-    {
-    my $parent = get_parent_id(\@path);
-    $insert_directory->execute($parent->{'id'},($parent->{'depth'} + 1),pop(@path));
+    $id = $root_result->{'id'};
     }
   else
     {
-    $insert_directory->execute(0,0,$root);
+    print "In here for $directory\n";
+    my $insert_root_sql = "insert into directories (name, parent, depth) values (?, 0, 0)";
+    my $insert_root_query = $db->prepare($insert_root_sql);
+    $insert_root_query->execute($directory);
+    $id = &last_id;
     }
+
+  while (scalar(@path))
+    {
+    my $find_parent_sql = "select id, parent, depth from directories where id = ?";
+    my $find_parent_query = $db->prepare($find_parent_sql);
+    $find_parent_query->execute($id);
+    my $parent = $find_parent_query->fetchrow_hashref;
+
+    #print Dumper \@path, "\n";
+    my $directory = shift(@path);
+
+    # test to see if exists
+    my $child_directory_sql = "select id from directories where name = ? and parent = ?";
+    my $child_directory_query = $db->prepare($child_directory_sql);
+    $child_directory_query->execute($directory, $parent->{'id'});
+
+    if (($id) = $child_directory_query->fetchrow_array) { }
+    else
+      {
+      my $insert_child_sql = "insert into directories (name, parent, depth) values (?, ?, ?)";
+      my $insert_child_query = $db->prepare($insert_child_sql);
+      $insert_child_query->execute($directory, $parent->{'id'}, ($parent->{'depth'} + 1));
+      $id = &last_id;
+      }
+    }
+
+  return $id;
   }
 
-sub get_parent_id
+sub last_id
   {
-  my $path = shift;
-  my $up_depth = shift // 1;
-  $get_directory->execute($root,0);
-  my $parent = $get_directory->fetchrow_hashref;
-  for (my $x = 0; $x < (scalar(@{$path})-$up_depth); $x++)
-    {
-    $get_directory->execute($path->[$x], $parent->{'id'});
-    $parent = $get_directory->fetchrow_hashref;
-    }
-  print "Parent ID: ", $parent->{'id'}, "\n";
-  return $parent;
+  my $last_id_sql = "SELECT LAST_INSERT_ID()";
+  my $last_id_query = $db->prepare($last_id_sql);
+  $last_id_query->execute();
+  my ($id) = $last_id_query->fetchrow_array;
+  return $id;
   }
+
+
