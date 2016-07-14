df542b91176b9ded6207b58e722fbbb8d96319ed
Frank Hereford <frank@frankhereford.com>
2016-07-14 13:02:39 -0500

    actual directory maker that doesn't fail

--- zfs_file_index.pl
+++ zfs_file_index.pl
@@ -24,8 +24,6 @@ sub do_not_dwell_on_what_has_passed_away_or_what_is_yet_to_be
   $db->do('TRUNCATE files;'); # it's TOTALLY smart to include TRUNCATE commands right at the top of your code. This has NEVER made me curse and holler. Never.
   $db->do('TRUNCATE directories;');
 
-
-
   my @filesystems = ();
   my $cmd = 'zfs list -o mountpoint -t filesystem';
   open(my $zfs, "-|", $cmd);
@@ -81,8 +79,6 @@ sub where_is_my_gypsy_wife_tonight
     {
     my $id = &my_house_in_the_middle_of_the_street($name);
     print $id, ": ", $name, "\n";
-    #<>;
-    #&insert_directory($name); # insert directory
     }
   #elsif (-e $name and -r $name)
     #{
@@ -111,6 +107,61 @@ sub stubborn_as_hose_garbage_bags_that_time_cannot_decay # check to make sure we
 
 
 
+sub my_house_in_the_middle_of_the_street
+  {
+  #print "Welcome to zombocom\n";
+  my $path = shift;
+  my @path = split("/", $path);
+  my $zeroth_before_the_slash_nothing_lived_here_ignore_this_long_variable_the_compiler_hates_us_both = shift(@path);
+  my $directory = shift(@path);
+  my $root_sql = "select id from directories where name = ? and depth = ?"; #" and parent = ?";
+  my $root_query = $db->prepare($root_sql);
+  $root_query->execute($directory, 0);
+  my $root_result = $root_query->fetchrow_hashref;
+  my $id;
+  if ($root_result->{'id'})
+    {
+    $id = $root_result->{'id'};
+    }
+  else
+    {
+    my $insert_root_sql = "insert into directories (name, parent, depth) values (?, 0, 0)";
+    my $insert_root_query = $db->prepare($insert_root_sql);
+    $insert_root_query->execute($directory);
+    $id = &last_id;
+    }
+
+  while (scalar(@path))
+    {
+    my $find_parent_sql = "select id, parent, depth from directories where id = ?";
+    my $find_parent_query = $db->prepare($find_parent_sql);
+    $find_parent_query->execute($id);
+    my $parent = $find_parent_query->fetchrow_hashref;
+    my $directory = shift(@path);
+    my $child_directory_sql = "select id from directories where name = ? and parent = ?";
+    my $child_directory_query = $db->prepare($child_directory_sql);
+    $child_directory_query->execute($directory, $parent->{'id'});
+    if (($id) = $child_directory_query->fetchrow_array) { }
+    else
+      {
+      my $insert_child_sql = "insert into directories (name, parent, depth) values (?, ?, ?)";
+      my $insert_child_query = $db->prepare($insert_child_sql);
+      $insert_child_query->execute($directory, $parent->{'id'}, ($parent->{'depth'} + 1));
+      $id = &last_id;
+      }
+    }
+  return $id;
+  }
+
+sub last_id
+  {
+  my $last_id_sql = "SELECT LAST_INSERT_ID()";
+  my $last_id_query = $db->prepare($last_id_sql);
+  $last_id_query->execute();
+  my ($id) = $last_id_query->fetchrow_array;
+  return $id;
+  }
+
 sub occurrences 
   {
   # tip of hat: http://stackoverflow.com/users/74585/matthew-lock
@@ -129,71 +180,4 @@ sub occurrences
 
   return $matches;
   }
-
-sub my_house_in_the_middle_of_the_street
-  {
-  #print "Welcome to zombocom\n";
-  my $path = shift;
-  #print $path, "\n";
-  my @path = split("/", $path);
-  my $zeroth_before_the_slash_nothing_lived_here_ignore_this_long_variable_the_compiler_hates_us_both = shift(@path);
-  my $directory = shift(@path);
-  #print Dumper \@path, "\n";
-  my $root_sql = "select id from directories where name = ? and depth = ?"; #" and parent = ?";
-  my $root_query = $db->prepare($root_sql);
-  #print "Args: '", $directory, "'\n";
-  $root_query->execute($directory, 0);
-  my $root_result = $root_query->fetchrow_hashref;
-  #print Dumper $root_result, "\n";
-  my $id;
-  if ($root_result->{'id'})
-    {
-    $id = $root_result->{'id'};
-    }
-  else
-    {
-    print "In here for $directory\n";
-    my $insert_root_sql = "insert into directories (name, parent, depth) values (?, 0, 0)";
-    my $insert_root_query = $db->prepare($insert_root_sql);
-    $insert_root_query->execute($directory);
-    $id = &last_id;
-    }
-
-  while (scalar(@path))
-    {
-    my $find_parent_sql = "select id, parent, depth from directories where id = ?";
-    my $find_parent_query = $db->prepare($find_parent_sql);
-    $find_parent_query->execute($id);
-    my $parent = $find_parent_query->fetchrow_hashref;
-
-    #print Dumper \@path, "\n";
-    my $directory = shift(@path);
-
-    # test to see if exists
-    my $child_directory_sql = "select id from directories where name = ? and parent = ?";
-    my $child_directory_query = $db->prepare($child_directory_sql);
-    $child_directory_query->execute($directory, $parent->{'id'});
-
-    if (($id) = $child_directory_query->fetchrow_array) { }
-    else
-      {
-      my $insert_child_sql = "insert into directories (name, parent, depth) values (?, ?, ?)";
-      my $insert_child_query = $db->prepare($insert_child_sql);
-      $insert_child_query->execute($directory, $parent->{'id'}, ($parent->{'depth'} + 1));
-      $id = &last_id;
-      }
-    }
-
-  return $id;
-  }
-
-sub last_id
-  {
-  my $last_id_sql = "SELECT LAST_INSERT_ID()";
-  my $last_id_query = $db->prepare($last_id_sql);
-  $last_id_query->execute();
-  my ($id) = $last_id_query->fetchrow_array;
-  return $id;
-  }
-
 
